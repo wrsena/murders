b7afc35b34af0b301257c6022ff6c2508fa110dc
wagner <wagnersena@fisica.ufc.br>
2019-02-20 16:18:27 -0300

    Adicionando todos os arquivos.

---
+++ wrangle-data.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+murders <- read.csv("data/murders.csv")
+
+murders <- murders %>% mutate(region = factor(region), rate = total / population * 10^5)
+
+save(murders, file = "rda/murders.rda")
